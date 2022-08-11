80dcfd1791bd8329d3f690a7fdb80264ea761dff
Ernest-Low <ernestlow32@gmail.com>
2022-08-12 01:22:51 +0800

    Vercel deployment failed idk why

--- screens/modules/actiontext.js
+++ screens/modules/actiontext.js
@@ -2,21 +2,21 @@
 
 const $actionText = (inputtext, time, window = $("#actiontext")) => {
   //  Text input
-    const textarray = [];
-    textarray.push(inputtext);
-    let textPosition = 0;
-    //  1000ms = 1s, length / (time to render)
-    const speed = Math.floor(1000 / (textarray[0].length / time));
+  const textarray = [];
+  textarray.push(inputtext);
+  let textPosition = 0;
+  //  1000ms = 1s, length / (time to render)
+  const speed = Math.floor(1000 / (textarray[0].length / time));
 
-    //  Inner rewriting text
-    const innertypewriter = () => {
-      window.text(textarray[0].substring(0, textPosition));
-      if (textPosition++ != textarray[0].length) {
-        setTimeout(innertypewriter, speed);
-      }
-    };
+  //  Inner rewriting text
+  const innertypewriter = () => {
+    window.text(textarray[0].substring(0, textPosition));
+    if (textPosition++ != textarray[0].length) {
+      setTimeout(innertypewriter, speed);
+    }
+  };
 
-    innertypewriter();
+  innertypewriter();
 };
 
 export default $actionText;
