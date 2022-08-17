b7b2fb8c6ff02739f25770f01d3d3ce7f7d1b6a7
Ernest-Low <ernestlow32@gmail.com>
2022-08-17 10:01:34 +0800

    trying with github url for image

--- screens/mainscreen.js
+++ screens/mainscreen.js
@@ -3,8 +3,6 @@ import player from "../data_files/data_player.js";
 import zones from "../data_files/data_zone";
 import create_battle from "./modules/create_battle";
 
-import backgrounds from "/assets/image_data/backgrounds/Game_Landing_Page.png"
-
 //* Render Mainscreen (Aka main)
 const mainScreen = () => {
   //  Back blackscreen
@@ -25,7 +23,7 @@ const mainScreen = () => {
     "align-items": "flex-end",
     "justify-content": "center",
     "z-index": 1,
-    "background-image": `url("/assets/image_data/backgrounds/Game_Landing_Page.png")`,
+    "background-image": `url("https://github.com/Ernest-Low/project1/tree/main/assets/image_data/backgrounds/Game_Landing_Page.png")`,
     "background-size": "100% 100%",
     "background-repeat": "no-repeat",
     "object-fit": "fill",
